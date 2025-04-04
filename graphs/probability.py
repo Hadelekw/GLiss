d9bc831706013f38a70975d6460f95e1c93982f6
Maksymilian Wiekiera <maksymilian3563@gmail.com>
2025-04-04 20:31:03 +0200

    SA probability graphing

--- graphs/probability.py
+++ graphs/probability.py
@@ -11,9 +11,21 @@ from algorithms import temperatures, sa_settings
 FUNCTIONS = ['a', 'c', 'k']
 
 
+LINE_STYLES = ['k-', 'k:', 'k--']
+
+
 def main() -> None:
+    fig, axes = plt.subplots(ncols=2)
+    axes[0].set_xlabel('Iteration number [n]')
+    axes[0].set_ylabel('Temperature parameter value')
+    axes[0].set_title('(a)', fontsize=16)
+    axes[0].set_aspect(10)
+    axes[1].set_xlabel('Iteration number [n]')
+    axes[1].set_ylabel('Solution acceptance probability')
+    axes[1].set_title('(b)', fontsize=16)
+    axes[1].set_aspect(1000)
     x = range(sa_settings.NUMBER_OF_ITERATIONS)
-    for function_identifier in FUNCTIONS:
+    for style, function_identifier in zip(LINE_STYLES, FUNCTIONS):
         func = temperatures.TEMPERATURE_UPDATE_MAP[function_identifier]
         if function_identifier == 'a':
             ts = []
@@ -24,8 +36,17 @@ def main() -> None:
                     ts.append(func(sa_settings.INITIAL_TEMPERATURE, sa_settings.COOLING_RATE))
             y = [math.exp(-1/t) for t in ts]
         else:
-            y = [math.exp(-1/func(sa_settings.INITIAL_TEMPERATURE, sa_settings.FINAL_TEMPERATURE, i, sa_settings.NUMBER_OF_ITERATIONS)) for i in x]
-        plt.plot(x, y)
+            y = []
+            ts = [func(sa_settings.INITIAL_TEMPERATURE, sa_settings.FINAL_TEMPERATURE, i, sa_settings.NUMBER_OF_ITERATIONS) for i in x]
+            for t in ts:
+                if t:
+                    y.append(math.exp(-1/t))
+                else:
+                    y.append(0)
+        axes[0].plot(x, ts, style)
+        axes[1].plot(x, y, style)
+    axes[0].legend(['A', 'B', 'C'])
+    axes[1].legend(['A', 'B', 'C'])
     plt.show()
 
 
